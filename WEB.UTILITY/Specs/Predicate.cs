6c2a9e48e9252f70ab2fb2090402439efd19a449
JM <josemiguel.sevilla@abbott.com>
2025-10-10 08:27:54 -0500

    @escreen-tms #Specs

---
+++ WEB.UTILITY/Specs/Predicate.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace WEB.UTILITY.Specs
+{
+    public static class Predicate
+    {
+        public static Expression<Func<T, bool>> Or<T>(
+            this Expression<Func<T, bool>> expr1,
+            Expression<Func<T, bool>> expr2)
+        {
+            return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, expr2.Body), expr1.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(
+            this Expression<Func<T, bool>> expr1,
+            Expression<Func<T, bool>> expr2)
+        {
+            var param = expr1.Parameters[0];
+            if (ReferenceEquals(param, expr2.Parameters[0]))
+            {
+                return Expression.Lambda<Func<T, bool>>(
+                    Expression.AndAlso(expr1.Body, expr2.Body), param);
+            }
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(
+                    expr1.Body,
+                    Expression.Invoke(expr2, param)), param);
+        }
+    }
+}
