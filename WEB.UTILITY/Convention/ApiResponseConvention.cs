9e2a1efd9031478a8de3486dbd156e4bdcdbdf04
mycp14 <johnmichaelcabanes@gmail.com>
2025-10-13 01:03:13 +0800

    escreen-tms modify apiresponseconvention based on the centralized apiresponse

--- WEB.UTILITY/Convention/ApiResponseConvention.cs
+++ WEB.UTILITY/Convention/ApiResponseConvention.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using WEB.UTILITY.Helper;
 
 namespace WEB.UTILITY.Convention
 {
@@ -8,21 +9,22 @@ namespace WEB.UTILITY.Convention
     {
         public void Apply(ApplicationModel application)
         {
+            var responseType = typeof(ApiResponse<object>);
+
             foreach (var controller in application.Controllers)
             {
                 foreach (var action in controller.Actions)
                 {
-                    // Add default response types if not already defined
-                    if (!action.Filters.OfType<ProducesResponseTypeAttribute>().Any())
-                    {
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status200OK));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status400BadRequest));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status201Created));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status403Forbidden));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status404NotFound));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status401Unauthorized));
-                        action.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status500InternalServerError));
-                    }
+                    if (action.Filters.OfType<ProducesResponseTypeAttribute>().Any())
+                        continue;
+
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status200OK));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status201Created));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status400BadRequest));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status401Unauthorized));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status403Forbidden));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status404NotFound));
+                    action.Filters.Add(new ProducesResponseTypeAttribute(responseType, StatusCodes.Status500InternalServerError));
                 }
             }
         }
