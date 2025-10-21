f5849b5c400962d8eadac7f2b2195b8ed800da44
John Michael Cabanes <johnmichael.cabanes@abbott.com>
2025-10-21 08:37:47 -0500

    escreen-tms userDto fluentvalidator

--- WEB.SERVICES/Validation/UserDtoValidator.cs
+++ WEB.SERVICES/Validation/UserDtoValidator.cs
@@ -1,14 +1,45 @@
 ﻿using FluentValidation;
+using WEB.DOMAIN.Entity;
+using WEB.DOMAIN.Interface;
 using WEB.SERVICES.DTO;
 
 namespace WEB.SERVICES.Validation
 {
     public class UserDtoValidator : AbstractValidator<UserDto>
     {
-        public UserDtoValidator()
+        private readonly IRepository<Auth> _authRepository;
+        private readonly IRepository<UserInfo> _userInfoRepository;
+
+        public UserDtoValidator(IRepository<Auth> authRepository, IRepository<UserInfo> userInfoRepository)
         {
+            _authRepository = authRepository;
+            _userInfoRepository = userInfoRepository;
+
             RuleFor(x => x.RoleID)
                 .NotEmpty().WithMessage("Role ID is required.");
+
+            RuleFor(x => x.Auth.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MustAsync(BeUniqueUsername).WithMessage("Username must be unique.");
+
+            RuleFor(x => x.Auth.Password)
+                .NotEmpty().WithMessage("Password is required.");
+
+            RuleFor(x => x.UserInfo.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.")
+                .MustAsync(BeUniqueEmail).WithMessage("Email must be unique.");
+        }
+
+        private async Task<bool> BeUniqueUsername(string username, CancellationToken ct)
+        {
+            var existing = (await _authRepository.GetAllAsync(u => u.Username == username, ct)).Any();
+            return !existing;
+        }
+        private async Task<bool> BeUniqueEmail(string email, CancellationToken ct)
+        {
+            var existing = (await _userInfoRepository.GetAllAsync(u => u.Email == email, ct)).Any();
+            return !existing;
         }
     }
 }
