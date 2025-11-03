10222f98c5c4833031016ec15dd1ea3dd14dfd48
Alexander Ablaza <alexander.ablaza@abbott.com>
2025-11-03 13:05:10 -0600

    @escreen-tms #add gateway gateway crud api

--- WEB.GATEWAY/Constants.cs
+++ WEB.GATEWAY/Constants.cs
@@ -6,4 +6,5 @@ public static class Constants
 {
     public const string RATE_LIMIT_POLICY_METADATA_KEY = "RateLimitPolicy";
     public const string REVERSE_PROXY = "ReverseProxy";
+    public const string GATEWAY = "Gateway";
 }
